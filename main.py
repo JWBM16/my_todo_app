45c4dcf901beca27d5e405d84e5c5f1338a0d9c0
JWBM16 <mypythonadventure@gmail.com>
2024-10-09 20:08:15 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fasthtml.common import *
+
+
+def render(todo):
+    tid = f"todo-{todo.id}"
+    toggle = A("Toggle", hx_get=f"/toggle/{todo.id}", target_id=tid)
+    delete = A("Delete", hx_delete=f"/{todo.id}", hx_swap="outerHTML", target_id=tid)
+    return Li(toggle, delete, todo.title + ("✅" if todo.done else ""), id=tid)
+
+
+app, rt, todos, Todo = fast_app(
+    "todos.db", live=False, render=render, id=int, title=str, done=bool, pk="id"
+)
+
+
+def mk_input():
+    return Input(placeholder="Add a New todo", id="title", hx_swap_oob="true")
+
+
+@rt("/")
+def get():
+    frm = Form(
+        Group(mk_input(), Button("Add")),
+        hx_post="/",
+        target_id="todo-list",
+        hx_swap="beforeend",
+    )
+    return Titled("Developer Todos", Card(Ul(*todos(), id="todo-list"), header=frm))
+
+
+@rt("/{tid}")
+def post(todo: Todo):
+    return todos.insert(todo)
+
+
+@rt("/{tid}")
+def delete(tid: int):
+    todos.delete(tid)
+
+
+@rt("/toggle/{tid}")
+def get(tid: int):
+    todo = todos[tid]
+    todo.done = not todo.done
+    return todos.update(todo)
+
+
+serve()
